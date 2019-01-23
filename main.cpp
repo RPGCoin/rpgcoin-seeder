aa941a0d9afdaa157170faa74281a794293a00a3
FrostyRPG <frosty@rpgcoin.gg>
2019-01-22 20:56:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -453,10 +453,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x0b;
-      pchMessageStart[1] = 0x11;
-      pchMessageStart[2] = 0x09;
-      pchMessageStart[3] = 0x07;
+      pchMessageStart[0] = 0x74;
+      pchMessageStart[1] = 0x93;
+      pchMessageStart[2] = 0x7A;
+      pchMessageStart[3] = 0x54;
       seeds = testnet_seeds;
       fTestNet = true;
   }
